2da847d3db1db2683bff8b2823164d473ba31521
joao969 <joao_marcos3@hotmail.com>
2024-03-13 17:34:41 -0300

    attfinal

--- javascript.js
+++ javascript.js
@@ -6,3 +6,9 @@ $('.galeria').click(function() {
   return false;
 });
 
+$(document).ready(function() {
+	$('#subir').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
