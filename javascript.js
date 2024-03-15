2372b77b8eb45168c097e20033f62879a56deea1
joao969 <joao_marcos3@hotmail.com>
2024-03-15 18:16:56 -0300

    attFinal1

--- javascript.js
+++ javascript.js
@@ -12,3 +12,18 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+
+const sr= ScrollReveal({
+  distance :'20%',
+  duration: 2000,
+  delay: 5,
+  reset:true
+});
+sr.reveal('.pag1',{delay:-600, origin:'left'});
+sr.reveal('.titulo',{delay:-300, origin:'left'});
+sr.reveal('.titulo2',{delay:-300, origin:'right'});
+sr.reveal('.imagens1',{delay:-600, origin:'right'});
+sr.reveal('.borda',{delay:-600, origin:'right'});
+sr.reveal('.minigaleria',{delay:-700, origin:'top'});
+sr.reveal('.logo',{delay:-200, origin:'left'});
